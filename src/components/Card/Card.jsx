17207edac74ca96caefe3836994e2960182a4598
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2023-01-25 17:40:52 -0600

    working on makePalette

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,25 +4,49 @@ import './Card.css'
 
 const Card = () => {
     const [randomColor, setRandomColor] = useState("#ffffff")
+    const [colorArray, setColorArray] = useState([])
     
-    const generateColor = () => {
+    // const generateColor = () => {
+    //     let hexColor = Math.random().toString(16).substring(2,8)
+    //     document.body.style.backgroundColor = `#${hexColor}`;
+    //     setRandomColor(`#${hexColor}`)
+    //     return randomColor
+    // }
+    // let generatedColor = generateColor()
+
+    const makePalette = () => {
         let hexColor = Math.random().toString(16).substring(2,8)
-        document.body.style.backgroundColor = `#${hexColor}`;
-        setRandomColor(`#${hexColor}`)
-        return randomColor
-    }
+        if (colorArray.length < 5) {
+            setRandomColor(`#${hexColor}`)
+            setColorArray([randomColor, ...colorArray])
+        }
+
+
+
+
+        for (let i = 0; i < 5; i++) {
+                console.log("colorArray: ", colorArray)
+                return colorArray
+            }
+        }
+
+
+   
 
     return (
         <div className='card-wrapper'>
             <h1>Random Hex Color Gen</h1>
             
-            <div className='hex-wrapper' style={{backgroundColor: randomColor}}>
-                <h2 className='hex'>{randomColor}</h2>
+            <div className='hex-wrapper' >
+                <h2 className='hex'></h2>
                 <PaletteBox />
             </div>
             <hr className='divider'></hr>
             <button className='button'
-                onClick={() => {generateColor()}}
+                onClick={() => {
+                    makePalette()
+                    
+                    }}
             >
                 🎨 Generate Color
             </button>
@@ -31,3 +55,5 @@ const Card = () => {
 }
 
 export default Card
+
+// style={{backgroundColor: randomColor}}
--- src/components/PaletteBox/PaletteBox.jsx
+++ src/components/PaletteBox/PaletteBox.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorBox from "../ColorBox/ColorBox";
 import './PaletteBox.css'
 
 const PaletteBox = () => {
+    // const [randomColor, setRandomColor] = useState("#df8e8e")
+
+    // const generateColor = () => {
+    //     let hexColor = Math.random().toString(16).substring(2,8)
+    //     setRandomColor(`#${hexColor}`)
+    //     return randomColor
+    // }
+    
+    // let colorArray = []
+    // const makePalette = () => {
+    //     for (let i = 0; i < 5; i++) {
+    //         colorArray.push(generateColor())
+    //     }
+    // }
+
+    // console.log("colorArray: ", colorArray)
+
     return (
         <div>
             <ColorBox />
