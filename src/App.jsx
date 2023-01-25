135863517382acaae91a80598d011d742f871c9e
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2023-01-25 15:42:21 -0600

    add palette components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import './App.css'
-import Card from './Card'
+import Card from './components/Card/Card'
+
+
+//card will contain the paletteBox and generator button
+//PaletteBox will contain five ColorBox components
+//need heart button to save colors
+//ColorBox will each be a randomly generated color
+//SavedColors will hold saved colors from each paletteBox
+//savedcolors should have a trash can to remove colors
+
 
 function App() {
 
---
+++ src/components/ColorBox/ColorBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './ColorBox.css'
+
+const ColorBox = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default ColorBox
